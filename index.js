100ea7ef26b15965c5b952b363d6b18d621b4e50
Yusei Ito <yuseikun@Outlook.jp>
2020-04-09 22:33:17 +0900

    Change port

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const http = require('http');
+const http = require("http");
 
-const hostname = 'localhost';
-const port = 3000;
+const hostname = "localhost";
+const port = 80;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World. Your node app is now running successfully.');
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello World. Your node app is now running successfully.");
 });
 
 server.listen(port, hostname, () => {
